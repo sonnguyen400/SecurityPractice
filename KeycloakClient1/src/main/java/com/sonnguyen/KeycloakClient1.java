c512efe55ce2e69d8a6e1b4d5f4db39bf09345ff
Nguyễn Hoàng Sơn <sonnguyen9616@gmail.com>
2024-11-19 14:46:44 +0700

    2

---
+++ KeycloakClient1/src/main/java/com/sonnguyen/KeycloakClient1.java
@@ -0,0 +1,11 @@
+package com.sonnguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakClient1 {
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakClient1.class, args);
+    }
+}
