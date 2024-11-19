6b93cd8c9bf2072ec35296d1c1a3463c2dbb9795
Nguyen Hoang Son <sonnguyen9616@gmail.com>
2024-11-19 09:16:20 +0700

    cmmit

---
+++ KeycloakPractice/src/main/java/com/sonnguyen/KeycloakPractice.java
@@ -0,0 +1,13 @@
+package com.sonnguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+@SpringBootApplication
+public class
+KeycloakPractice{
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakPractice.class, args);
+    }
+}
---
+++ Oauth2ResourceServer/src/main/java/com/sonnguyen/Oauth2ResourceServer.java
@@ -0,0 +1,11 @@
+package com.sonnguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ResourceServer {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ResourceServer.class, args);
+        }
+}
