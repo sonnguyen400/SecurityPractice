46a5094aa9ef2e6cfd78e74b2bbc8b07f5879804
Nguyen Hoang Son <sonnguyen9616@gmail.com>
2024-11-26 23:03:39 +0700

    ok

---
+++ KeyCloakTest/src/main/java/com/sonnguyen/KeycloakTest.java
@@ -0,0 +1,11 @@
+package com.sonnguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakTest {
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakTest.class, args);
+    }
+}
